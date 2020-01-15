f043ff30c3eb3468b86ff0442cddfa4bd786302a
Richard Weerasinghe <rich.w@me.com>
2020-01-16 08:36:02 +1100

    added account_id output

--- cloudability-linked-account/outputs.tf
+++ cloudability-linked-account/outputs.tf
@@ -1,3 +1,8 @@
+output "account_id" {
+  description = "Cloudability AWS Account ID"
+  value       = cloudability_master_account.aws_payer_account.vendor_account_id
+}
+
 output "external_id" {
   description = "Cloudability AWS Linked Account External ID"
   value       = cloudability_linked_account.aws_account.external_id
--- cloudability-master-account/outputs.tf
+++ cloudability-master-account/outputs.tf
@@ -1,3 +1,8 @@
+output "account_id" {
+  description = "Cloudability AWS Account ID"
+  value       = cloudability_master_account.aws_payer_account.vendor_account_id
+}
+
 output "external_id" {
   description = "Cloudability AWS Account External ID"
   value       = cloudability_master_account.aws_payer_account.external_id
