4f9c7029c22daa7d65dbcff0a742ca0a0212d97d
Richard Weerasinghe <rweeras@amazon.com>
2021-09-22 21:36:45 +1200

    updated main.tf to reference cloudability v0.0.7

--- cloudability-linked-account/main.tf
+++ cloudability-linked-account/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    cloudability = {
+      source = "skyscrapr/cloudability"
+      version = "0.0.7"
+    }
+  }
+}
+
 provider cloudability {
     apikey = var.cloudability_apikey
 }
