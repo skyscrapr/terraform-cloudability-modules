5e608b9d23f5871fe1c913559eb2c75bb1c92eb9
Richard Weerasinghe <rweeras@amazon.com>
2021-09-23 17:35:10 +1200

    added costcenters

--- cloudability-business-mappings/business_mapping_tag_compliance.tf
+++ cloudability-business-mappings/business_mapping_tag_compliance.tf
@@ -11,31 +11,120 @@ resource "cloudability_business_mapping" "cost_center" {
         value_expression = "'CostCenter2'"
     }
     statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter3'"
+        value_expression = "'CostCenter3'"
+    }
+    statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter4'"
+        value_expression = "'CostCenter4'"
+    }
+    statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter5'"
+        value_expression = "'CostCenter5'"
+    }
+    statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter6'"
+        value_expression = "'CostCenter6'"
+    }
+        statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter7'"
+        value_expression = "'CostCenter7'"
+    }
+    statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter8'"
+        value_expression = "'CostCenter8'"
+    }
+    statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter9'"
+        value_expression = "'CostCenter9'"
+    }
+    statement {
+        match_expression = "TAG['CostCenter'] == 'CostCenter10'"
+        value_expression = "'CostCenter10'"
+    }
+    statement {
         match_expression = "EXISTS TAG['CostCenter']"
         value_expression = "'Invalid CostCenter Tag'"
     }
 }
 
-# resource "cloudability_view" "test_view" {
-#     title = "View"
-# }
-
 resource "cloudability_view" "CostCenter_CostCenter1" {
     title = "CostCenter1"
     filter {
-        # This prefix might need to be coded in the provider or sdk
         field = "category${cloudability_business_mapping.cost_center.id}"
 		comparator = "=="
 		value = "CostCenter1"
     }
 }
-
 resource "cloudability_view" "CostCenter_CostCenter2" {
     title = "CostCenter2"
     filter {
-        # This prefix might need to be coded in the provider or sdk
         field = "category${cloudability_business_mapping.cost_center.id}"
 		comparator = "=="
 		value = "CostCenter2"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter3" {
+    title = "CostCenter3"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter3"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter4" {
+    title = "CostCenter4"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter4"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter5" {
+    title = "CostCenter5"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter5"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter6" {
+    title = "CostCenter6"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter6"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter7" {
+    title = "CostCenter7"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter7"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter8" {
+    title = "CostCenter8"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter8"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter9" {
+    title = "CostCenter9"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter9"
+    }
+}
+resource "cloudability_view" "CostCenter_CostCenter10" {
+    title = "CostCenter10"
+    filter {
+        field = "category${cloudability_business_mapping.cost_center.id}"
+		comparator = "=="
+		value = "CostCenter10"
     }
 }
