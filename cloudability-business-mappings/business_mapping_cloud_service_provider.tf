16c0b2d9df3c39118d6009d4d4c0aa7c6ae571cf
Richard Weerasinghe <rweeras@amazon.com>
2021-09-23 14:03:48 +1200

    added provider requirements

--- cloudability-business-mappings/business_mapping_cloud_service_provider.tf
+++ cloudability-business-mappings/business_mapping_cloud_service_provider.tf
@@ -1,14 +1,14 @@
 resource "cloudability_business_mapping" "cloud_service_provider" {
     name = "Cloud Service Provider"
-    default_value = "Unknown Cloud Service Provider"
+    default_value = "Unknown"
     kind = "BUSINESS_DIMENSION"
     statement {
         match_expression = "DIMENSION['vendor'] == 'Amazon'"
         value_expression = "'Amazon'"
     }
     statement {
-        match_expression = "DIMENSION['vendor'] == 'Azure'"
-        value_expression = "'Azure'"
+        match_expression = "DIMENSION['vendor'] == 'Other1'"
+        value_expression = "'Other1'"
     }
 }
 
--- cloudability-business-mappings/business_mapping_tag_compliance.tf
+++ cloudability-business-mappings/business_mapping_tag_compliance.tf
@@ -1,17 +1,17 @@
-# resource "cloudability_business_mapping" "tag_compliance" {
+# resource "cloudability_business_mapping" "costcentre_tag" {
 #     name = "Tag Compliance"
 #     default_value = "Untagged"
 #     kind = "BUSINESS_DIMENSION"
 #     statement {
-#         match_expression = "TAG['Cost Centre'] == 'valid_tag_1' || TAG['Cost Centre'] == 'valid_tag_1'"
-#         value_expression = "'Correctly Tagged'"
+#         match_expression = "TAG['CostCentre'] == 'CostCentre1' || TAG['CostCentre'] == 'CostCentre2'"
+#         value_expression = "'Valid Cost Centre'"
 #     }
 #     statement {
 #         match_expression = "EXISTS TAG['Cost Centre']"
-#         value_expression = "'Incorrectly Tagged'"
+#         value_expression = "'Invalid Cost Centre Allocated'"
 #     }
 # }
 
-# resource "cloudability_view" "test_view" {
-#     title = "Test-View"
-# }
+# # resource "cloudability_view" "test_view" {
+# #     title = "View"
+# # }
