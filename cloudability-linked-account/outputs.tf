b66f280de1ebdf6fcb387c142748cff086d6a543
Richard Weerasinghe <rweeras@amazon.com>
2021-09-23 16:34:05 +1200

    remove account id from outputs

--- cloudability-linked-account/outputs.tf
+++ cloudability-linked-account/outputs.tf
@@ -1,7 +1,7 @@
-output "account_id" {
-  description = "Cloudability AWS Account ID"
-  value       = cloudability_linked_account.aws_account.vendor_account_id
-}
+# output "account_id" {
+#   description = "Cloudability AWS Account ID"
+#   value       = cloudability_linked_account.aws_account.vendor_account_id
+# }
 
 output "external_id" {
   description = "Cloudability AWS Linked Account External ID"
